5b5291e06e620de106c8163e5f2cf42784689a95
Harsha <nerlapalleharshavardhanreddy@gmail.com>
2022-03-01 16:02:16 +0530

    commited

--- xyz.java
+++
@@ -1,9 +0,0 @@
-public class B{
-  public static void main(Strinh[] args){
-    System.out.println("hello");
-     System.out.println("hello");
-   int a=10;
-System.out.println(a);
-String s="MSD";
-}
-}
